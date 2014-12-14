6fb78e83a6a69fadaa73c3f3914ea78bdb43592b
Flemming Thesbjerg <flemming.thesbjerg@gmail.com>
2014-12-14 18:54:34 +0100

    add test to verify ignore relations

--- spec/seeder_spec.rb
+++ spec/seeder_spec.rb
@@ -12,4 +12,16 @@ describe GraphSeed::Seeder do
       "project_1 = Project.create!(employee: employee_1, name: \"Sms\")"
     ])
   end
+
+  it "Create seeds but without projects" do
+    gs = GraphSeed::Seeder.new(Company.first, ignored: { projects: true } )
+
+    seed = gs.to_seed
+
+    expect(seed).to eq([
+      "company_1 = Company.create!(name: \"Flightlogger\")",
+      "employee_1 = Employee.create!(company: company_1, name: \"Flemming\")"
+    ])
+
+  end
 end
